258033545dd746bae42792796d704447eaedc33f
Lucas <oliveira.lucas20@gmail.com>
2023-03-12 19:43:52 -0400

    Moves files to ui package

--- app/src/main/java/com/lucassimao/sorteianomesenumeros/ui/DrawNamesActivity.kt
+++ app/src/main/java/com/lucassimao/sorteianomesenumeros/ui/DrawNamesActivity.kt
@@ -1,7 +1,8 @@
-package com.lucassimao.sorteianomesenumeros
+package com.lucassimao.sorteianomesenumeros.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lucassimao.sorteianomesenumeros.R
 
 class DrawNamesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
